b9a34382c25015b90b78c39eee369a13eedec72e
Jordan Banasik <woovie@woovie.net>
2016-06-01 22:55:18 -0500

    First commit
    Modified a lot of the code to work with JSON instead of crawling HTML. It's significantly easier to get data from JSON.

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 'use strict';
 const Discord = require('discord.js'), //Discord module
-    cheerio = require("cheerio"),
     request = require("request");
 
 var discord = new Discord.Client(); //New discord client
 var trigger_prefix = "!ow";
-var heroes_to_display = 5;
 
-discord.loginWithToken("token"); //Login using a Discord 'BOT' account token
-//discord.login("email", "password"); //Login with a normal user account
+discord.loginWithToken(""); //Login using a Discord 'BOT' account token
 
 discord.on('ready', () => { //The discord client has connected
     console.log("Successful connection to Discord as '" + discord.user.username + "'");
@@ -17,26 +14,25 @@ discord.on('ready', () => { //The discord client has connected
 discord.on('message', (message) => { //A message was sent in a Discord channel
     if (message.author.id == discord.user.id) return; //Exit if message is from self(bot)
     if (!message.content.startsWith(trigger_prefix)) return; //Exit if the message doesn't start with our trigger word
-    let tag = message.content.substring(trigger_prefix.length, message.length).replace('#', '-'); //Remove trigger prefix and replace hash with hyphen
+    let tag = message.content.substring(trigger_prefix.length, message.length).trim(); //Remove trigger prefix and remove space
     if (tag == "") return discord.sendMessage(message, "``" + trigger_prefix + " name#id``"); //Show usage if no name is passed
-    let url = encodeURI("https://playoverwatch.com/en-us/career/pc/us/" + tag + "/"); //TODO: handle other regions
-    let name = tag.split('-')[0]; //Get name from tag for messages
-    request.get({url: url}, (err, res, body) => { //Get HTML
-        if (err || res.statusCode != 200) return discord.sendMessage(message, "Error getting stats for ``" + name + "``");
-        let $ = cheerio.load(body); //Load HTML string as jQuery object
-        let data = []; //List to hold hero data
-        let rank = $('.player-level').text(); //Scraped player rank
-        let heroes = $('.progress-category')[0]; //Gets first set of drop down hero data (Time Played)
-        for (let i = 0; i < heroes_to_display; i++) {
-            let hero = $(heroes).children()[i]; //Gets hero data row
-            let name = $(hero).find('.title').text(); //Scrapes hero name
-            let time = $(hero).find('.description').text(); //Scrapes time played
-            data.push({name: name, time: time}); //Pushes hero to data list
+    let url = encodeURI("https://api.watcher.gg/players/pc/us/" + tag).replace("#", "%23"); //URL for grabbing player data from API
+    let url_pub = encodeURI("https://watcher.gg/profile/pc/us/" + tag).replace("#", "%23"); //URL for public linking
+    let name = tag.split('#')[0]; //Get name from tag for messages
+    request.get(url, function (err, res, body) { //Get the URL contents
+        let data = JSON.parse(body); //Parse the contents to a JSON table
+        let code = data.statusCode; //Get the status code for error checking
+        if ( code != 200 ) { //If we get something other than a 200 OK
+            return discord.sendMessage(message, "Error getting stats for ``" + name + "``");
+        } else {
+            let str = "Watcher.gg Stats for " + tag + "\n";
+            str += "```Score/sec: " + String ( Math.round ( data.data.heroStats[0].score / data.data.heroStats[0].timePlayed * 1000 ) / 1000 ) + " Rank: #"  + numberWithCommas ( data.data.ranks[0].ranks.averageScore ) + "\n"; //Calculate score per second and display player rank overall for US
+            str += "Total Score: " + numberWithCommas ( data.data.heroStats[0].score ) + " Total Time Played: ~" + data.data.heroStats[0].timePlayed/60/60 + " hours\n"; //Display raw score and hours played
+            str += "View the full page here: " + url_pub; //Send a URL at the end to the watcher.gg page of this player
+            return discord.sendMessage ( message, str );
         }
-        let str = "```" + name + (name.endsWith("s") ? "'" : "'s") + " Overwatch level is: " + rank + ". Top heroes are: ```"; //Top line
-        for (let i = 0; i < data.length; i++) {
-            str += "\n``" + (i + 1) + ".``**" + data[i].name + "** - " + data[i].time; //Enumerated stats
-        }
-        discord.sendMessage(message, str);
     });
 });
+function numberWithCommas(number) { //This function turns 999999999 into 999,999,999
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
