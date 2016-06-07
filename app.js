3e7b8ae11ff30e8c80ced184359cc8bbdfa277e4
Jordan Banasik <woovie@woovie.net>
2016-06-06 20:05:04 -0500

    First commit
    Added https library
    Added sleep library
    Redesigned URL building
    Modified events to launch external function instead of inline for cleaner reading
    Added some attempts to figure out a solution for async URL processing to determine the user's region
    Added error handling function to send appropriate discord messages

--- app.js
+++ app.js
@@ -7,81 +7,75 @@ Github here: https://github.com/dbkynd/discord-overwatch-stats
 Forked by Woovie to work with watcher.gg API https://api.watcher.gg/players/pc/us/woovie%231582
 Github here: https://github.com/Woovie/discord-overwatch-stats
 draft build
+Anything marked with //*** is merely for testing different ideas and may be removed later.
 */
 
-'use strict';
-const Discord = require('discord.js'),
-request = require("request");
+"use strict";
+const Discord = require ("discord.js"),
+request = require ("request");
+const https = require ("https");//***
+const sleep = require ("sleep");//***
 
-//Global variables
+//Discord variables
 var discord = new Discord.Client();
-discord.loginWithToken(""); //Login using a Discord 'BOT' account token
+discord.loginWithToken (""); //Login using a Discord 'BOT' account token
 
 //Overwatch specific variables
 var overwatch_prefix = "!ow";
+var base_url = "watcher.gg/players/pc/";
+var api_prefix = "https://api.";
+var pub_prefix = "https://";
+var regions = ["us", "eu", "kr"];
+var region;
+var tag;
+var final_url;
+var urls_checked = 0;//***
 
-//Global functions
-function numberWithCommas(number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function round000(number) {
-    return (Math.round(number * 1000) / 1000);
-}
-
-discord.on('ready', () => { //The discord client has connected
-    console.log("Successful connection to Discord as '" + discord.user.username + "'");
-    discord.setPlayingGame("watcherbot.net")
-});
-
-//Overwatch stats function
-discord.on('message', (message) => {
+//Handle incoming message
+function discordMessage (message) {
     if (message.author.id == discord.user.id) return;
     if (!message.content.startsWith(overwatch_prefix)) return;
-    let tag = message.content.substring(overwatch_prefix.length, message.length).trim();
-    if (tag == "") return discord.sendMessage(message, "``" + overwatch_prefix + " name#id``");
-    let url = encodeURI("https://api.watcher.gg/players/pc/us/" + tag).replace("#", "%23");
-    let url_pub = encodeURI("https://watcher.gg/profile/pc/us/" + tag).replace("#", "%23");
-    let name = tag.split('#')[0];
-    request.get(url, function (err, res, body) {
-        let data = JSON.parse(body);
-        let code = data.statusCode;
-        if ( code != 200 ) {
-            return discord.sendMessage(message, "Error getting stats for ``" + name + "``");
-        } else {
-            let player = [];
-            player.level = data.data.player.level;
-            player.lastUpdated = data.data.player.lastUpdated;
-            player.score = data.data.heroStats[0].score;
-            player.timePlayed = data.data.heroStats[0].timePlayed;
-            player.scorePerSecond = player.score / player.timePlayed;
-            player.timePlayedHours = player.timePlayed / 3600;
-            player.heroes = [];
-            player.heroes.sortedByScore = [];
-            for (let i=0; i < data.data.heroStats.length; i++) {
-                if (data.data.heroStats[i].score == 0) { continue };
-                if (data.data.heroStats[i].id == -1) { continue };
-                player.heroes.sortedByScore.push(data.data.heroStats[i]);
+    tag = message.content.substring(overwatch_prefix.length, message.length).trim();
+    if (tag == "") { return tagError(message, -1) };
+    var region_url = api_prefix + base_url;
+    for (var i=0; i<regions.length; i++) {
+        var test_url = region_url + regions[i] + "/" + tag.replace ("#", "%23");
+        https.get (test_url, function (response) {//***
+            if (response.statusCode === 200) {
+                processRegionURL (test_url);
+
             }
-            player.heroes.sortedByScore.sort(function(a,b){return b.score-a.score});
-            if (data.data.ranks[0].ranks.averageScore == -1) { player.scoreRank = "Unranked" } else { player.scoreRank = "#" + numberWithCommas(data.data.ranks[0].ranks.averageScore) };
-            let str = "Watcher.gg Stats for **" + tag + "** last updated on " + player.lastUpdated + "\n";
-            str += "**Score/sec:** " + round000(player.scorePerSecond) + " **Rank:** "  + player.scoreRank + "\n";
-            str += "**Total Score:** " + numberWithCommas(player.score) + " **Total Time Played:** ~" + player.timePlayedHours + " hours\n";
-            if (player.heroes.sortedByScore.length >= 3) {
-                str += "**Top 3 Heroes by Total Score:**\n";
-                for (let i=0;i < 3; i++) {
-                    str += "**" + (Number(i) + 1) + ".** " + player.heroes.sortedByScore[i].name + " **Score:** " + numberWithCommas(player.heroes.sortedByScore[i].score);
-                    for (let j=0;j < data.data.ranks.length; j++) {
-                        if (data.data.ranks[j].id == player.heroes.sortedByScore[i].id) {
-                            if (data.data.ranks[j].ranks.averageScore != -1) { str += " **Rank** #" + numberWithCommas(data.data.ranks[j].ranks.averageScore)};
-                        };
-                    };
-                    str += "\n";
-                };
-            };
-            str += "\nView the full page here: " + url_pub;
-            return discord.sendMessage ( message, str );
-        }
-    });
-});
+            urls_checked++;//***
+        })
+    }
+    while (urls_checked != 3) {//***
+
+    }
+    return processUser (message);
+}
+
+function processUser (message) {
+    if (final_url) {
+        discord.sendMessage (message, "Start of a found user to process " + final_url);
+    } else {
+        tagError (message, 1);
+    }
+}
+
+//Region found
+function processRegionURL (test_url) {//***
+    final_url = test_url;
+}
+
+//Overwatch error with tag
+function tagError (message, err) {
+    if (err == -1) {
+        discord.sendMessage (message, "Please ensure you included a battletag, no battletag was found.");
+    } else if (err == 1) {
+        discord.sendMessage (message, "No user found by the battletag **" + tag + "**, please check the spelling.");
+    }
+}
+
+//Overwatch events
+discord.on ('ready', discordReady);
+discord.on ('message', discordMessage);
